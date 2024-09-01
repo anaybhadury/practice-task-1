e264c94706ba82b76adabf85fa6400d170b2b4c0
anaybhadury <bhadury9292@gmail.com>
2024-09-01 22:34:21 +0600

    added practice task 4

---
+++ task-4.js
@@ -0,0 +1,19 @@
+/**
+ * --------Practice Task-----------
+ * What will be the result of the following codes:
+ * var a = isNaN(‘11’);
+ * var a = isNaN(2-10);
+ * Explain your answers.
+ */
+
+// var a = isNaN('11');
+// console.log(a);
+
+var b = 2 - 10;
+
+console.log(b);
+
+var total = isNaN(8);
+
+console.log(total);
+
