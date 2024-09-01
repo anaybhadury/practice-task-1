e25b0e6f6e08d24e4d8ffc420a001da62a4bcffb
anaybhadury <bhadury9292@gmail.com>
2024-09-01 22:19:21 +0600

    added practice task 3

---
+++ task-3.js
@@ -0,0 +1,27 @@
+/**
+ * --------Practice Task-----------
+ * Your task is to divide the given number by 5 and show the remainder as the output.
+ * Input: The first line of the input contains the number.
+ * Output: Print the remainder.
+ * Sample Input: 119
+ * Sample Output: 4
+ */
+
+var x = 119;
+var total = x % 5;
+console.log(total);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
